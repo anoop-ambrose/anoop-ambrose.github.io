ae8c44ab1f0a0c373ae75b263d9c222bf8c499cc
Anoop Ambrose <itsmeanoop4536@gmail.com>
2026-06-24 23:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const text = [
+    "Aspiring Penetration Tester",
+    "Web Application Security",
+    "Vulnerability Assessment",
+    "Linux Enthusiast"
+];
 
+let textIndex = 0;
+let charIndex = 0;
+
+function typeEffect() {
+
+    const typing = document.getElementById("typing");
+
+    if(charIndex < text[textIndex].length){
+
+        typing.textContent += text[textIndex].charAt(charIndex);
+        charIndex++;
+
+        setTimeout(typeEffect,100);
+
+    }else{
+
+        setTimeout(() => {
+
+            typing.textContent = "";
+
+            charIndex = 0;
+
+            textIndex = (textIndex + 1) % text.length;
+
+            typeEffect();
+
+        },1500);
+    }
+}
+
+typeEffect();
